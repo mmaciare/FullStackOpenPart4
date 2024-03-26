e5a2ad3842a9e96a4ac010296e67feedd8513a2b
marta <mmaciareyes@gmail.com>
2024-03-26 12:35:14 +0100

    4.9

--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -18,6 +18,12 @@ test('there are 5 blogs', async () => {
   assert.strictEqual(response.body.length, 5)
 })
 
+test.only('the unique identifier is id and not _id', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+  assert('id' in blogs[0] && !('_id' in blogs[0]))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
